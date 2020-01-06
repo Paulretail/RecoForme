6b6bf13cf45669d5bd0a4125834e46172ff7cea4
Richard <yanisrichard@live.fr>
2020-01-06 11:18:39 +0100

    calcul de distance

--- kPPV.py
+++ kPPV.py
@@ -1,5 +1,7 @@
+# coding: utf-8
 """ programme à compléter du kPPV"""
 import csv
+import math as m
 
 nbExParClasse = 50
 nbApprent = 25
@@ -22,15 +24,19 @@ def lectureFichierCSV():
 def calculDistances(data, dataset):
     """ retourne les distances entre data et la partie apprentissage de dataset"""
     distances = []
-    
-# -------- A faire... --------
+    for i in range(25) + range(50,75) + range(100,125):
+        sum = 0
+        for j in range(4):
+            sum += (data[j]-dataset[i][j])**2
+        distances.append(m.sqrt(sum))
+
     return(distances)
 
 
 def calculClasse(distances):
     """ retourne le numéro de la classe déterminé à partir des distances """
-    
-# -------- A faire... --------
+
+
 
     return(classe)
 
@@ -38,6 +44,8 @@ def calculClasse(distances):
 if __name__ == "__main__":
     print("Début programme kPPV")
     dataset = lectureFichierCSV()
+
+    print(len(calculDistances([1.1,1.1,1.1,1.1], dataset)))
 
     # Calcule et affiche la matrice de confusion et le taux de reco
     
