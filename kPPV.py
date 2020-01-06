358ccafaae87f09b9e5db87fe210581858980e26
Paul <paul.retail@me.com>
2020-01-06 11:38:29 +0100

    merge

--- kPPV.py
+++ kPPV.py
@@ -18,19 +18,31 @@ def lectureFichierCSV():
         for j in range(nbCaract):
             dataset[i][j] = float(dataset[i][j])
     # print(dataset[0])
-    return(dataset)
+    return dataset
 
 
 def calculDistances(data, dataset):
     """ retourne les distances entre data et la partie apprentissage de dataset"""
     distances = []
-    for i in range(25) + range(50,75) + range(100,125):
+    for i in range(25):
         sum = 0
         for j in range(4):
             sum += (data[j]-dataset[i][j])**2
         distances.append(m.sqrt(sum))
 
-    return(distances)
+    for i in range(50, 75):
+        sum = 0
+        for j in range(4):
+            sum += (data[j] - dataset[i][j]) ** 2
+        distances.append(m.sqrt(sum))
+
+    for i in range(100, 125):
+        sum = 0
+        for j in range(4):
+            sum += (data[j] - dataset[i][j]) ** 2
+        distances.append(m.sqrt(sum))
+
+    return distances
 
 
 def calculClasse(distances):
@@ -38,7 +50,7 @@ def calculClasse(distances):
 
     index = distances.index(min(distances))
 
-    return int(index / 25)
+    return int(index/25)
 
 
 if __name__ == "__main__":
